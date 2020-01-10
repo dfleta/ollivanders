c1b692c74a29ceb07883d6762407ee2c32c24665
dfleta <gelpiorama@gmail.com>
2020-01-10 19:36:13 +0100

    feat(controller.py): Implementar api rest basica
    Implementar 3 endpoints para entender su funcionamiento

--- controller.py
+++ controller.py
@@ -5,12 +5,26 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class Ollivanders(Resource):
+
     def get(self):
-        return {'hello': 'world'}
+        return {'Welcome!': 'Ollivanders'}
 
 
-api.add_resource(HelloWorld, '/')
+class Inventario(Resource):
+
+    def get(self):
+        return {'Inventario': 'Ollivanders'}
+
+
+class UpdateQuality(Resource):
+
+    def get(self):
+        return {'updated': 'Items'}
+
+api.add_resource(Ollivanders, '/')
+api.add_resource(Inventario, '/inventario')
+api.add_resource(UpdateQuality, '/updateQuality')
 
 if __name__ == '__main__':
     app.run(debug=True)
